fe9c4c10b7d42598162d4bee9094592fbefce2de
Luigi Pepe <luigipepe90@gmail.com>
2018-03-01 20:15:29 -0500

    first commit

---
+++ Genio/Login-Signup/Login.swift
@@ -0,0 +1,19 @@
+//
+//  Login.swift
+//  Genio
+//
+//  Created by Luigi Pepe on 2/28/18.
+//  Copyright © 2018 Luigi Pepe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    
+    
+    
+}
+
+
--- Genio/ViewController.swift
+++ Genio/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kj: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       // kj.spacing
+        
+        
     }
     
     
