86369967256ec693284865aa9638dd7c85a229f8
Luigi Pepe <luigipepe90@gmail.com>
2018-03-11 12:55:44 -0400

    move text fields on the logo when typing input

--- Genio/Login-Signup/Login.swift
+++ Genio/Login-Signup/Login.swift
@@ -11,14 +11,14 @@ import UIKit
 
 class LoginViewController : UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
     
-    
+    //#MARK: Varible Declarations
     let back_color_ = UserDefaults.standard.color(forKey: "backColor")
     let labels_color_ = UserDefaults.standard.color(forKey: "labelsColor")
     let text_color_ = UserDefaults.standard.color(forKey: "textColor")
     var token = String()
     var original_stack_bottom_const_ : CGFloat = 0.0
     
-    /*********************************************/
+    //#MARK: Coupling Variables
     @IBOutlet weak var signup_button_: UIButton!
     @IBOutlet weak var login_button_: UIButton!
     @IBOutlet weak var activity_indicator_: UIActivityIndicatorView!
@@ -31,78 +31,96 @@ class LoginViewController : UIViewController, UITextFieldDelegate, UIImagePicker
     @IBOutlet weak var stack_view_: UIView!
     @IBOutlet weak var stack_view_top_distance_: NSLayoutConstraint!
     @IBOutlet weak var logo_top_const_: NSLayoutConstraint!
-    /*********************************************/
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
         self.view.backgroundColor = labels_color_
         self.stack_view_.backgroundColor = labels_color_
+        
         UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: text_color_!], for: .normal)
         UITabBarItem.appearance().setTitleTextAttributes([NSAttributedStringKey.foregroundColor: labels_color_!], for: .selected)
+        
         setupLogo()
-        setupBottomStack()
-        setUpinputFields()
+        //setupBottomStack()
+        setupInputFields()
     }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
     
+    
+    //MARK: Setup Graphics
     func setupLogo(){
         let image = UIImage(named: "Genio_Logo")
         self.logo_.image =  image?.maskWithColor(color: UIColor.white)
-        self.logo_.tintColor = UIColor.white
+        //self.logo_.tintColor = UIColor.white
     }
-    
-    func setupBottomStack(){
-        self.login_button_.titleLabel?.font = UIFont(name: "BrandonGrotesque-Regular", size: 18)
-        self.login_button_.setTitle("LOG IN", for: .normal)
-        self.login_button_.setTitleColor(UIColor.white, for: .normal)
-        self.login_button_.backgroundColor = UIColor.clear
-        self.signup_button_.titleLabel?.font = UIFont(name: "BrandonGrotesque-Regular", size: 18)
-        self.signup_button_.setTitle("SIGN UP", for: .normal)
-        self.signup_button_.setTitleColor(UIColor.white, for: .normal)
-        self.signup_button_.backgroundColor = UIColor.clear
-    }
-    
-    
-    
-    func setUpinputFields() {
+    func setupInputFields() {
+        
+        var height: CGFloat = self.view.bounds.height * 0.3
+        
+        if (self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClass.regular &&
+            self.traitCollection.verticalSizeClass == UIUserInterfaceSizeClass.regular) {
+            self.stack_view_top_distance_.constant = 160
+            self.main_stack_.spacing = 70
+            height += 100
+        }
+        
         self.email_text_field_.delegate = self
         self.email_text_field_.setLeftPaddingPoints(10)
         self.email_text_field_.setRightPaddingPoints(10)
-        self.email_text_field_.layer.cornerRadius = (((self.view.bounds.height * 0.3) - stack_view_top_distance_.constant - (self.main_stack_.spacing * 2)) / 3) / 2
+        self.email_text_field_.layer.cornerRadius =  ((height - stack_view_top_distance_.constant - (self.main_stack_.spacing * 2)) / 3) / 2
         self.email_text_field_.layer.borderColor = UIColor.white.cgColor
+        
         self.psw_text_field_.delegate = self
         self.psw_text_field_.setLeftPaddingPoints(10)
         self.psw_text_field_.setRightPaddingPoints(10)
-        self.psw_text_field_.layer.cornerRadius = (((self.view.bounds.height * 0.3) - stack_view_top_distance_.constant - (self.main_stack_.spacing * 2)) / 3) / 2
+        
+        
+        
+        self.psw_text_field_.layer.cornerRadius = ((height - stack_view_top_distance_.constant - (self.main_stack_.spacing * 2)) / 3) / 2
+        
+        print(self.main_stack_height_.constant )
+        print(self.stack_view_top_distance_.constant)
+        print(self.psw_text_field_.bounds.height )
+        print(self.main_stack_.spacing)
         self.psw_text_field_.layer.borderColor = UIColor.white.cgColor
+        
         self.activity_indicator_.stopAnimating()
         self.activity_indicator_.hidesWhenStopped = true
     }
     
     
-    
-    
-    
+    //#MARK: Action
     @IBAction func loginAction(_ sender: UIButton) {
-        
          print("login")
-        
-        
     }
     
     @IBAction func signupAction(_ sender: UIButton) {
         print("sign up")
-        
     }
     
+    
+    //MARK: Textfield actions/graphics
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.original_stack_bottom_const_ = self.main_stack_bottom_const_.constant
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.curveLinear], animations: {
+            self.main_stack_bottom_const_.constant = (self.view.bounds.height - self.logo_top_const_.constant - self.view.safeAreaInsets.top - self.stack_view_.bounds.height)
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.curveLinear], animations: {
+            self.main_stack_bottom_const_.constant = self.original_stack_bottom_const_
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    
+        //self.view.endEditing(true)
+    }
+
     func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -112,25 +130,9 @@ class LoginViewController : UIViewController, UITextFieldDelegate, UIImagePicker
         textField.resignFirstResponder()
         return true
     }
-    
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        self.original_stack_bottom_const_ = self.main_stack_bottom_const_.constant
-        UIView.animate(withDuration: 0.3, delay: 0.0, options: [.curveLinear], animations: {
-            self.main_stack_bottom_const_.constant = (self.view.bounds.height - self.logo_top_const_.constant - self.view.safeAreaInsets.top - self.stack_view_.bounds.height)
-            self.view.layoutIfNeeded()
-        }, completion: nil)
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-            UIView.animate(withDuration: 0.3, delay: 0.0, options: [.curveLinear], animations: {
-                self.main_stack_bottom_const_.constant = self.original_stack_bottom_const_
-                self.view.layoutIfNeeded()
-            }, completion: nil)
-        
-            self.view.endEditing(true)
-        }
-    }
+}
+
+
     
     
 
